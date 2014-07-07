f19c1d76f3684358eb4813a6aa9fd409ce91f9bf
sq123 <kakaby13@mail.ru>
2014-07-08 02:47:21 +0400

    3
    создан объект

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿using System;                       
-using System.Collections.Generic;   
-using System.Linq;                  
-using System.Text;                  
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+
 
 namespace обучалка
 {
@@ -16,20 +17,24 @@ namespace обучалка
         int maxfuel = 10;
         int height = 0;
         int maxheight = 900;
+        bool engine = false;
 
         int ind; //это индекс объекта. это временная переменная, которую можно удалить, если реализуешь иначе. но она пока для того, что бы вызов метода "респаун" не ругался.***
 
-        public airport(int startfuel)
+
+        /*public airport(int startfuel)
         {
             fuel = startfuel;
             Console.WriteLine("Диспетчерская. Система управления самолетами.");
-        }
+
+        }*/
         public void getfuel()  // заправка     
         {
             fuel = maxfuel;
         }
         public void morespeed(int a)   // увелич. скорости
         {
+
             fuel = fuel - a * maxfuel * 5 / 100;  //на каждое изм. скорости требуется 5 проц от макс кол-ва топлива
             if (speed + a < maxspeed)
             {
@@ -39,6 +44,7 @@ namespace обучалка
             {
                 Console.WriteLine("Нельзя увеличить скорость на данное значение, превышает макс. скорость.");
             }
+
         }
         public void lessspeed(int a)   // уменьш. скорости
         {
@@ -82,7 +88,7 @@ namespace обучалка
                 {
                     height = 0;
                     Console.WriteLine("Самолет разбился.");
-                    Program.RespawnPlane(ind);   //передаём индекс объекта, который нужно перезаполнить******
+                    //Program.RespawnPlane(ind);   //передаём индекс объекта, который нужно перезаполнить******
                 }
                 else
                 {
@@ -91,10 +97,20 @@ namespace обучалка
                 }
             }
         }
+        public void startengine()
+        {
+            engine = true;
+            Console.WriteLine("Запуск двигателя.");
+        }
+        public void stopengine()
+        {
+            engine = false;
+            Console.WriteLine("Отключение двигателя.");
+        }
     }
     class Program  //базовый класс
-    {
-        //
+    {      
+        
         //коллекция Авиапарк объектов класса самолёт
         //
         static void StartProgramm(int kol_planes)       //метод, создающий объекты самолёт и заносящий их в Авиапарк. принимаемый параметр = количеству самолётов в авиапарке
@@ -104,7 +120,7 @@ namespace обучалка
                 CreatePlane(i);                         //
             }                                           //
         }
-        public static void RespawnPlane(int n)         //метод для респауна самолётов. вызывается при взрыве и тд. передаётся индекс объекта, который требуется заменить
+        static void RespawnPlane(int n)         //метод для респауна самолётов. вызывается при взрыве и тд. передаётся индекс объекта, который требуется заменить
         {
             CreatePlane(n);
             //
@@ -113,9 +129,19 @@ namespace обучалка
         }
         static void CreatePlane(int n)             //заполнение/вызов конструктора класса Самолёт
         {
-            //
-            //
-            //
+            //airport plane = new airport(n);
+        }
+        static void center()
+        {
+            int command = Convert.ToInt32(Console.ReadLine());
+
+            switch (command)
+            {
+                case 1:
+                    
+                    
+
+            }
         }
 
         static void Main(string[] args) //точка входа в программу
@@ -125,14 +151,28 @@ namespace обучалка
             StartProgramm(kol_planes);      //вызов метода стартпрограмм с передачей количества самолётов
 
 
+            airport p = new airport();
+            p.startengine();
+
+
+            
+
+
+            Console.WriteLine("Введите команду (1 - запуск двиг., 2 - остановка двиг., 3 - заправка, 4 - увелич. скорость, 5 - уменьшить скорость, 6 - увелич. высоту, 7 - уменьш. высоту). ");
+            center();
+
+
             //cl cl1 = new cl(n);         //создание экземпляра класса. тут мы используем конструктор с параметром
             //cl cl2 = new cl();          //тут конструктор без параметров
+
 
             //Console.WriteLine(cl1.MetN());      //
             //Console.WriteLine(cl2.MetN());      //вызов методов 1 у обоих классов
 
+
             //cl1.MetM(5);        //
             //cl2.MetM(5);        //вызов второго метода у обоих экземпляров класса 
+
 
             Console.WriteLine("финиш");
             Console.ReadKey();
